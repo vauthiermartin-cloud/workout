56af67d14967d1119fa1d64a68f29abad8ed80ab
vauthiermartin-cloud <vauthier.martin@gmail.com>
2026-09-01 14:14:28 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "workout-v15";
+const CACHE = "workout-v16";
 const CORE = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png", "./apple-touch-icon.png"];
 
 self.addEventListener("install", (e) => {
